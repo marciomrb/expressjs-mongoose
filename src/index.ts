ae09737f188dcd92c9c461a58ca038136407d7b1
Marcio Jr <marciomrbcampos@gmail.com>
2022-11-15 16:23:25 -0300

    db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import "./lib/db";
 import express from "express";
-import countryRoutes from "./routes/country";
+import cors from "cors";
+import { router } from './router';
+
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(cors());
+app.use('/public', express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
+app.use(router);
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
@@ -13,7 +18,7 @@ app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
 });
 
-app.use("/countries", countryRoutes);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/routes.ts
@@ -0,0 +1,78 @@
+
+import { Router } from 'express';
+import path from 'node:path';
+import multer from 'multer';
+
+// category
+import { createCategory } from './useCases/categories/createCategory';
+import { listCategories } from './useCases/categories/listCategories';
+import { createProduct } from './useCases/products/createProduct';
+
+//product
+import { listProducts } from './useCases/products/listProducts';
+import { listProductsByCategory } from './useCases/categories/listProductsByCategory';
+
+//order
+import { createOrders } from './useCases/orders/createOrders';
+import { listOrders } from './useCases/orders/listOrders';
+import { changeOrderStatus } from './useCases/orders/changeOrderStatus';
+import { cancelOrder } from './useCases/orders/cancelOrder';
+import { listUsers } from './useCases/users/listUsers';
+
+// user
+import { createUsers } from './useCases/users/createUsers';
+import { getUserById } from './useCases/users/getUserById';
+import { updateUser } from './useCases/users/updateUser';
+import { deleteUser } from './useCases/users/deleteUser';
+
+
+export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    }
+  })
+});
+
+// list categories
+router.get('/categories', listCategories);
+// create category
+router.post('/categories', createCategory);
+
+// list products
+router.get('/products', listProducts);
+
+// create product
+router.post('/products', upload.single('image'), createProduct);
+
+// get product by category
+router.get('/categories/:categoryId/products', listProductsByCategory);
+
+// list orders
+router.get('/orders', listOrders);
+
+// create order
+router.post('/orders', createOrders);
+
+// change order status
+router.patch('/orders/:orderId', changeOrderStatus);
+
+// delete/cancel order
+router.delete('/orders/:orderId', cancelOrder);
+
+
+// list users
+router.get('/users', listUsers);
+// create user
+router.post('/users', createUsers);
+// get user by id
+router.get('/users/:userId', getUserById);
+// update user
+router.patch('/users/:userId', updateUser);
+// delete user
+router.delete('/users/:userId', deleteUser);
