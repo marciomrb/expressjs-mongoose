c231dca7ed6675532ecdd5b463ceb03656207eaa
Marcio Jr <marciomrbcampos@gmail.com>
2022-11-15 17:24:42 -0300

    multer cloudinary

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,8 @@
 import { Router } from 'express';
 import path from 'node:path';
 import multer from 'multer';
-
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+const cloudinary = require("cloudinary").v2;
 // category
 import { createCategory } from './useCases/categories/createCategory';
 import { listCategories } from './useCases/categories/listCategories';
@@ -25,19 +26,34 @@ import { getUserById } from './useCases/users/getUserById';
 import { updateUser } from './useCases/users/updateUser';
 import { deleteUser } from './useCases/users/deleteUser';
 
+cloudinary.config({ 
+  cloud_name: 'ddugqkgko', 
+  api_key: '486868118642275', 
+  api_secret: 'F0PL1AGdjsb2od0Xd3TaBjXCkSk',
+  secure: true
+});
 
 export const router = Router();
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, '..', 'uploads'));
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}-${file.originalname}`);
-    }
-  })
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, path.resolve(__dirname, '..', 'uploads'));
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, `${Date.now()}-${file.originalname}`);
+//     }
+//   })
+// });
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "styla",
+  },
 });
+
+const upload = multer({ storage: storage });
 
 // list categories
 router.get('/categories', listCategories);
