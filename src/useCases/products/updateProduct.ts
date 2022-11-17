b3be78a00b2c45fcbbb8529f637965cdf8340ace
Marcio Jr <marciomrbcampos@gmail.com>
2022-11-17 12:44:20 -0300

    image path

--- src/useCases/products/updateProduct.ts
+++ src/useCases/products/updateProduct.ts
@@ -4,7 +4,7 @@ import { Product } from '../../models/Product';
 export async function updateProduct(req: Request, res: Response) {
     try {
         const imagePath = req.file?.path;
-        const { name, description, price, category } = req.body;
+        const { name, description, price, category, image } = req.body;
         const { productId } = req.params;
     
         const product = await Product.findByIdAndUpdate(productId, {
@@ -12,7 +12,7 @@ export async function updateProduct(req: Request, res: Response) {
             description,
             price: Number(price),
             category,
-            imagePath: imagePath
+            imagePath: imagePath ? imagePath : image,
         });
 
         return res.status(200).json(product);
